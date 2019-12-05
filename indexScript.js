289003648786030b6837ea4cc0ed0995062a7464
Poreniyi <poreniyi@gmail.com>
2019-12-05 12:40:58 -0500

    Added hover changing color

--- indexScript.js
+++ indexScript.js
@@ -1,9 +1,39 @@
 const container=document.querySelector('#container');
 let cellArray=[];
+container.style.display='grid';
+container.style['grid-template-columns']='repeat(16, 100px)';
+container.style['grid-template-rows']='repeat(16, 60px)';
+
 for(let i=0;i<16;i++){
     const cell=document.createElement('div');
-    cell.textContent='Im cell #'+i;
-    cellArray[i]=cell;
-    container.appendChild(cell);
+    for(let j=0;j<16;j++){
+        cell.textContent="I'm cell#("+i+'/'+j+')';
+        cellArray[i]=cell;
+        container.appendChild(cell);
+        cell.style['grid-column']= i/j;
+        cell.style.color='white';
+        cell.style.backgroundColor='black';
+        //cell.setAttribute(':hover')
+       // cell.style[':hover']=cell.style.backgroundColor='white';
+    }
+    // cell.textContent='Im cell #'+i;
+    // cellArray[i]=cell;
+    // container.appendChild(cell);
 }
-console.log(cellArray);
+for(let i=0;i<cellArray.length;i++){
+    cellArray[i].addEventListener('mouseover',()=>{
+        onHover(cellArray[i]);
+    });
+    cellArray[i].addEventListener('mouseover',()=>{
+        onHover(cellArray[i]);
+    });
+console.log('This is#',i,cellArray[i]);
+}
+function onHover(item){
+    item.style.color='black';
+    item.style.backgroundColor='white';
+}
+function onExit(item){
+    
+}
+//console.log(cellArray);
